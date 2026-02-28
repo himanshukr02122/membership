57006dd9fd6f29c842c3a9f0f317847fa0c0c403
himanshukr02122 <himanshukumar02122@gmail.com>
2026-02-28 23:12:55 +0530

    some changes in the database.py file.

--- database.py
+++ database.py
@@ -2,10 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQL_ALCHEMY_DATABASE_URL = "mysql+pymysql://root:Him%400646@127.0.0.1:3306/membership"
-
-engine = create_engine(SQL_ALCHEMY_DATABASE_URL)
-
-SessionLocal = sessionmaker(autocommit = False, autoflush=False, bind=engine)
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:Him%400646@localhost:3306/membership"
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
